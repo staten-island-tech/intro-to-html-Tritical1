46afa39a2f2ef2abfc4e106c981ec48005068214
Tritical1 <110200988+Tritical1@users.noreply.github.com>
2023-09-16 11:11:52 -0400

    so um basically
    i forgor to commit
    ended up working out because the result is way nicer

--- app/Scripts/activate.bat
+++ app/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\dmose\OneDrive\Documents\GitHub\intro-to-html-Tritical1\app
+set VIRTUAL_ENV=C:\Users\tivb\Documents\GitHub\intro-to-html-Tritical1\app
 
 if not defined PROMPT set PROMPT=$P$G
 
